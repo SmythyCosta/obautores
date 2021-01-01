68f946ebaf7f3832d7c5eb69eb660caa3ce3db2f
SmythyCosta <smythy.costa@hotmail.com>
2021-01-01 16:08:07 -0300

    create interface

---
+++ src/main/java/com/example/demo/service/AutorService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public interface AutorService<T> {
+
+	/**
+	 * Retorna uma lista paginada.
+	 * 
+	 * @param pageRequest
+	 * @return Page<T>
+	 */
+	Page<T> listarTodos(PageRequest pageRequest);
+	
+	/**
+	 * Persiste um Obj na base de dados.
+	 * 
+	 * @param id
+	 * @return Obj
+	 */
+	T persistir(T t);
+	
+	
+	/**
+	 * Retorna um Obj por ID.
+	 * 
+	 * @param id
+	 * @return Optional<Entity>
+	 */
+	Optional<T> buscarPorId(Long id);
+	
+	/**
+	 * Retorna um Obj por nome.
+	 * 
+	 * @param nome
+	 * @return Optional<Entity>
+	 */
+	Optional<T> buscarPorNome(String nome);
+	
+	/**
+	 * Deleta um Obj base de dados.
+	 * 
+	 * @param id
+	 */
+	void remover(Long id);
+
+}
