5ba0710b6f77f573a6fc15373ff7ac4bf6419db4
SmythyCosta <smythy.costa@hotmail.com>
2021-01-19 05:54:12 -0300

    BaseController refactory

--- src/main/java/com/example/demo/controllers/BaseController.java
+++ src/main/java/com/example/demo/controllers/BaseController.java
@@ -1,22 +1,39 @@
 package com.example.demo.controllers;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import com.example.demo.exception.BusinessException;
 
-import com.example.demo.response.ErrorMessege;
+import com.example.demo.exception.BusinessException;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.response.ErrorMessegeResponse;
 
 public class BaseController {
 	
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<?> handlerBusinessException(BusinessException e) {
-		ErrorMessege errorMessege = errorMessege.builder()
-				.dateAndTime(LocalDateTime.now())
-				.menssage(e.getMessage()).build();
+		ErrorMessegeResponse errorMessege = setErrorMessegeResponse(e.getMessage());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(errorMessege);
 	}
+
+	
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<?> handlerNotFoundException(NotFoundException e) {
+		ErrorMessegeResponse errorMessege = setErrorMessegeResponse(e.getMessage());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(errorMessege);
+	}
+	
+	private ErrorMessegeResponse setErrorMessegeResponse(String message) {
+		ErrorMessegeResponse errorMessege = new ErrorMessegeResponse();
+		errorMessege.setTimestamp(LocalDateTime.now());
+		errorMessege.getErrors().add(message);
+		return errorMessege;
+	}
+	
 }
