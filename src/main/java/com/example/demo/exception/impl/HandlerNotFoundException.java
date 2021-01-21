7009a05d935c73db43d845f104b282c27ca7f776
SmythyCosta <smythy.costa@hotmail.com>
2021-01-21 00:03:55 -0300

    remove exception

--- src/main/java/com/example/demo/exception/impl/HandlerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception.impl;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class HandlerNotFoundException extends HandlerBusinessException {
-
-}
